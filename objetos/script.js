bba10e0e2573a5995a30b8301f2e09c491fd0a42
Javier Gonzalez <javgon2306@gmail.com>
2024-07-19 14:12:27 -0300

    first

---
+++ objetos/script.js
@@ -0,0 +1,4 @@
+const script =  document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
